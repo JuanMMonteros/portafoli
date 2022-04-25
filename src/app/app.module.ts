9e6e3835a8cbb546dad0bb2fdc2055b6e7f28343
JuanMMonteros <monterosjuan329@gmail.com>
2022-04-25 10:06:04 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SobreMiComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
